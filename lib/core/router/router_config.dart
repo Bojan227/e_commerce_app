45f704fa207a186c0e47ea7e160b8698548089df
Bojan227 <blazevski.bojan1@gmail.com>
2023-10-05 14:17:04 +0200

    add router config with auto route

---
+++ lib/core/router/router_config.dart
@@ -0,0 +1,23 @@
+import 'package:auto_route/auto_route.dart';
+
+import 'router_config.gr.dart';
+
+@AutoRouterConfig(replaceInRouteName: 'Page,Route')
+class AppRouter extends $AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(page: MainScreen.page, path: '/', children: [
+          AutoRoute(page: HomeNavWrapper.page, children: [
+            AutoRoute(page: HomeScreen.page, path: 'home', initial: true),
+            AutoRoute(
+              page: CategoryScreen.page,
+              path: 'category',
+            ),
+          ]),
+          AutoRoute(
+            page: BagScreen.page,
+            path: 'bag',
+          )
+        ]),
+      ];
+}
