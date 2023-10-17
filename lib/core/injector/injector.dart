5f406a8d0b6309f75b16495777f85dc85bb5a87e
Bojan227 <blazevski.bojan1@gmail.com>
2023-10-17 16:28:09 +0200

    add: injector with getIt

---
+++ lib/core/injector/injector.dart
@@ -0,0 +1,26 @@
+import 'package:ecommerce_app/core/sqflite/sqflite_helper.dart';
+import 'package:ecommerce_app/data/datasources/ulmo_local_datasource.dart';
+import 'package:ecommerce_app/data/repositories/ulmo_repository_impl.dart';
+import 'package:ecommerce_app/domain/repositories/ulmo_repository.dart';
+import 'package:ecommerce_app/domain/usecases/get_reviews_usecase.dart';
+import 'package:ecommerce_app/presentation/blocs/review/review_bloc.dart';
+import 'package:get_it/get_it.dart';
+
+GetIt getIt = GetIt.instance;
+
+Future setupInjector() async {
+  final SqfHelper sqfHelper = SqfHelper(dbName: 'ulmo_db.db');
+  getIt.registerSingleton<SqfHelper>(sqfHelper);
+
+  final UlmoLocalDataSource ulmoLocalDataSource =
+      UlmoLocalDataSourceImpl(sqfHelper: sqfHelper);
+  final UlmoRepository ulmoRepository =
+      UlmoRepositoryImpl(ulmoLocalDataSource: ulmoLocalDataSource);
+  final GetReviewsUseCase getReviewsUseCase =
+      GetReviewsUseCase(ulmoRepository: ulmoRepository);
+
+  final ReviewBloc reviewBloc =
+      ReviewBloc(getReviewsUseCase: getReviewsUseCase);
+
+  getIt.registerSingleton<ReviewBloc>(reviewBloc);
+}
